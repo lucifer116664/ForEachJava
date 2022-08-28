7121e540b53536e4b16b64f73f8bcd86f072c19f
lucifer116664 <102432996+lucifer116664@users.noreply.github.com>
2022-08-28 13:12:48 +0300

    Create ForEach.java

---
+++ ForEach.java
@@ -0,0 +1,13 @@
+public class ForEach
+{
+    public static void main(String[] args)
+    {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7};
+        int sum = 0;
+        for(int intEl:arr)
+        {
+            sum += intEl;
+        }
+        System.out.println(sum);
+    }
+}
